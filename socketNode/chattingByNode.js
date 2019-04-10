1bd66c4f79d43909cc931bc66483ce857d91e725
cyes7126 <yoo712695@gmail.com>
2019-04-10 18:31:25 +0900

    채팅

--- socketNode/chattingByNode.js
+++ socketNode/chattingByNode.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
-var moment = require('moment');
 var socketio = require('socket.io');
 
 
@@ -14,10 +13,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-var io = socketio();
+
 var server = require('http').createServer(app);
-io.attach(server);
+
 server.listen(52273);
+
+var io = socketio();
 /*
 var server = http.createServer(function (request, response){
     fs.readFile('socketPage.html', function(error, data){
@@ -28,39 +29,64 @@ var server = http.createServer(function (request, response){
     console.log('running...');
 });
 */
-app.get('/chat/:isbn',function(request,response){
-    fs.readFile('./socketPage2.html','utf-8',function(error,data){
-        db.chat.find(function(error,results){
-            response.send(ejs.render(data,{
-                data:results
-            }));;
+app.get('/chat', function (request, response) {
+    fs.readFile('./Test.html', 'utf-8', function (error, data) {
+        response.send(data);
+    });
+});
+
+
+app.post('/chat/:isbn', function (request, response) {
+    var body = request.body;
+    (function (error, result) {
+        response.send({
+            isbn: body.isbn,
+            bookTitle: body.bookTitle,
+            userName: body.userName
+        });
+    })();
+});
+
+
+
+app.get('/chat/:data', function (request, response) {
+    fs.readFile('./socketPage2.html', 'utf-8', function (error, data) {
+        var jsonData = JSON.parse(request.params.data);
+        db.chat.find({
+            isbn: jsonData.isbn
+        }, function (error, results) {
+            response.send(ejs.render(data, {
+                data: results,
+                isbn: jsonData.isbn,
+                bookTitle: jsonData.bookTitle,
+                userName: jsonData.userName
+            }));
+        });
+
+io.attach(server);
+        io.sockets.on('connection', function (socket) {
+            socket.on('join', function (data) {
+                socket.join(data);
+            });
+            //message이벤트
+            socket.on('message', function (data) {
+                console.log(data.userName+"의 메시지전송3");
+                db.chat.save({
+                    isbn: data.isbn,
+                    userName: data.userName,
+                    message: data.message,
+                    time: data.time
+                });
+                io.sockets.in(data.isbn).emit('message', data);
+                console.log(data.userName+"의 메시지전송4");
+            });
+            socket.on('first',function(data){
+               io.sockets.in(data.isbn).emit('first',data); 
+            });
+
+
         });
     });
+
 });
-
-app.post('/chat/:isbn',function(request,response){
-   var body = request.body;
-    io.sockets.on('connection',function(socket){
-    var roomName = null;
-    //join이벤트
-    socket.on('join',function(){
-       roomName=body.bookTitle;
-        socket.join(body.bookTitle);
-    });
-    //message이벤트
-    socket.on('message',function(data){
-        io.sockets.in(roomName).emit('message',data);
-        db.chat.save({
-        isbn:body.isbn,
-        userName: body.userName,
-        text:data,
-        time:moment().format('YYYY[-]MM[-]DD HH[:]mm')
-    });
-});
-    });
-});
-
-
-
-
-
+                /*io.sockets.in(jsonData.isbn).emit('message', data);*/
---
+++ socketNode/new.js
@@ -0,0 +1,126 @@
+var mongojs = require('mongojs');
+var db = mongojs('node');
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var socketio = require('socket.io');
+var cors = require('cors');
+var http = require('http');
+var najax = require('najax');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cors());
+
+var server = http.createServer(app).listen(52273,function(){
+    console.log('서버시작되었습니다.');
+});
+
+var io = socketio.listen(server);
+
+        io.sockets.on('connection', function (socket) {
+            socket.on('join', function (data) {
+                socket.join(data);
+            });
+            //message이벤트
+            socket.on('message', function (data) {
+                db.chat.save({
+                    isbn: data.isbn,
+                    userName: data.userName,
+                    message: data.message,
+                    time: data.time
+                });
+                io.sockets.in(data.isbn).emit('message', data);
+            });
+            socket.on('first',function(data){
+               io.sockets.in(data.isbn).emit('first',data); 
+            });
+
+
+        });
+/*app.post('/chat/message',function(socket){
+    io.sockets.in(data.isbn).emit('message', data);
+})*/
+
+app.post('/chat/:isbn', function (request, response) {
+    var body = request.body;
+    console.log(request.params.isbn);
+    (function (error, result) {
+        response.send({
+            isbn: body.isbn,
+            bookTitle: body.bookTitle,
+            userName: body.userName
+        });
+    })();
+});
+
+/*app.get('/chat/:data', function (request, response) {
+    fs.readFile('./mobile.html', 'utf-8', function (error, data) {
+        var jsonData = JSON.parse(request.params.data);
+        db.chat.find({
+            isbn: jsonData.isbn
+        }, function (error, results) {
+            response.send(ejs.render(data, {
+                data: results,
+                isbn: jsonData.isbn,
+                bookTitle: jsonData.bookTitle,
+                userName: jsonData.userName
+            }));
+        });
+
+    });
+
+});*/
+
+//app.get('/chat', function (request, response) {
+//    fs.readFile('./Test.html', 'utf-8', function (error, data) {
+//        response.send(data);
+//    });
+//});
+
+/*app.get('/chat/:data', function (request, response) {
+        var jsonData = JSON.parse(request.params.data);
+        db.chat.find({
+            isbn: jsonData.isbn
+        }, function (error, results) {
+            console.log("찾아낸 채팅목록");
+            var arr = {data:results, isbn:jsonData.isbn,
+                      bookTitle:jsonData.bookTitle,
+                      userName:jsonData.userName};
+            console.log(JSON.stringify(arr));
+            najax({               
+                url:'http://localhost/sentence/chat/'+jsonData.isbn,
+                type:'POST',
+                data:JSON.stringify(arr),
+                dataType:'json',
+                contentType:'application/json; charset=utf-8',
+                success:function(data){
+                    console.log('톰캣으로 전송');
+                    console.log(JSON.stringify(data));
+                },error:function(error){
+                    console.error();
+                }
+            });
+            
+            
+        });
+
+    });*/
+app.get('/chatRoom/:isbn', function (request, response) {
+    console.log('들어옴');
+        db.chat.find({
+            isbn: request.params.isbn
+        }, function (error, results) {
+
+            response.send({
+                data:results             
+             });
+            console.log("결과값:"+JSON.stringify(results));
+        });
+    });
+
